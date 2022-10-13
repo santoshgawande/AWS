41c5f6045ee02611d366f104cd94fa431aad4dba
Santosh Gawande <37923311+santoshgawande@users.noreply.github.com>
2022-10-13 07:43:24 +0530

    Update apache_ubuntu_bootstrap_script.sh

--- apache_ubuntu_bootstrap_script.sh
+++ apache_ubuntu_bootstrap_script.sh
@@ -2,4 +2,5 @@
 # First change user to root or use sudo for each step
 apt-get update -y
 apt install apache2
-echo "Hello World from $(hostname -f)"> /var/www/index.html
+echo "Hello World from $(hostname -f)"> /var/www/html/index.html
+service apache2 restart
